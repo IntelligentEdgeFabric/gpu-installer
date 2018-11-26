784697dfb72246e22752ba84d9a61863ac58870d
liulinghui <llhuii5614@gmail.com>
2018-11-26 10:46:40 +0800

    Add redhat support

--- nvidia-gpu-installer.sh
+++ nvidia-gpu-installer.sh
@@ -17,6 +17,12 @@ NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-384.111}"
 get_release()
 {
   eval "$(sed 's/^[A-Za-z]/OS_&/' /etc/os-release)"
+  if test $OS_ID = rhel; then
+    # translate redhat into centos
+    OS_ID=centos
+    # get the major number of OS_VERSION_ID
+    OS_VERSION_ID=$(echo $OS_VERSION_ID | cut -d. -f1)
+  fi
 }
 
 pre_check()
